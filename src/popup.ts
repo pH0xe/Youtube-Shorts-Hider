126c4cea3d8f62fccca6891d60e2376ee4f327c2
Julien Reig <julien.reig6@gmail.com>
2023-05-09 19:33:29 +0200

    registers html elements

--- src/popup.ts
+++ src/popup.ts
@@ -1,18 +1,47 @@
 import { StorageKey } from './enums/storageKey';
-import { StorageValue } from './enums/storageValue';
+import { TypeValue } from './enums/storageValue';
 import { StorageManagement } from './storage';
 
-const registerEventListeners = () => {
-  const options = document.querySelectorAll('.toggle');
-  options.forEach((option) => {
-    if (option instanceof HTMLInputElement)
-      option.addEventListener('click', saveOptions);
-  });
+const htmlElements = {
+  dimmingLevelElements: {
+    container: null as HTMLElement | null,
+    input: null as HTMLInputElement | null,
+    display: null as HTMLElement | null
+  },
+  toggleElements: {
+    enable: null as HTMLInputElement | null,
+    type: null as HTMLInputElement | null
+  }
+};
 
-  const dimmingLevel = document.getElementById(
-    'dimming-level'
+const queryElements = () => {
+  htmlElements.dimmingLevelElements.container = document.getElementById(
+    'dimming-level-container'
+  );
+  htmlElements.dimmingLevelElements.input = document.getElementById(
+    StorageKey.DIMMING_LEVEL
   ) as HTMLInputElement;
-  dimmingLevel.addEventListener('input', onDimmingLevelChange);
+  htmlElements.dimmingLevelElements.display =
+    document.getElementById('dimming-value');
+
+  htmlElements.toggleElements.enable = document.getElementById(
+    StorageKey.TOGGLE_ENABLE
+  ) as HTMLInputElement;
+
+  htmlElements.toggleElements.type = document.getElementById(
+    StorageKey.TOGGLE_TYPE
+  ) as HTMLInputElement;
+};
+
+const registerEventListeners = () => {
+  htmlElements.toggleElements.enable?.addEventListener('click', saveOptions);
+
+  htmlElements.toggleElements.type?.addEventListener('click', saveOptions);
+
+  htmlElements.dimmingLevelElements.input?.addEventListener(
+    'input',
+    onDimmingLevelChange
+  );
 };
 
 const saveOptions = (event: Event) => {
@@ -31,35 +60,32 @@ const saveOptions = (event: Event) => {
 };
 
 const restoreOptions = () => {
-  const enable = document.getElementById(
-    StorageKey.TOGGLE_ENABLE
-  ) as HTMLInputElement;
-  const type = document.getElementById(
-    StorageKey.TOGGLE_TYPE
-  ) as HTMLInputElement;
-
-  const dimmingLevelInput = document.getElementById(
-    'dimming-level'
-  ) as HTMLInputElement;
-  const dimmingLevelDisplay = document.getElementById('dimming-value');
-
   StorageManagement.isEnable().then((value) => {
-    enable.checked = value;
+    if (htmlElements.toggleElements.enable)
+      htmlElements.toggleElements.enable.checked = value;
   });
 
   StorageManagement.getType().then((value) => {
-    type.checked = value === StorageValue.HIDE;
+    if (htmlElements.toggleElements.type)
+      htmlElements.toggleElements.type.checked = value === TypeValue.HIDE;
+    if (value === TypeValue.DARKEN) hideDimmingLevel();
   });
 };
 
 const onDimmingLevelChange = (event: Event) => {
   const target = event.target as HTMLInputElement;
-  const dimmingLevelDisplay = document.getElementById('dimming-value');
+  const dimmingLevelDisplay = htmlElements.dimmingLevelElements.display;
   if (dimmingLevelDisplay)
     dimmingLevelDisplay.innerText = target.valueAsNumber.toString() + '%';
 };
 
+const hideDimmingLevel = () => {
+  const dimmingLevelInput = htmlElements.dimmingLevelElements.container;
+  if (dimmingLevelInput) dimmingLevelInput.style.display = 'none';
+};
+
 const init = () => {
+  queryElements();
   registerEventListeners();
   restoreOptions();
 };
